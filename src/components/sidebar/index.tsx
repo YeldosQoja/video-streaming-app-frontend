b3a83e27934ed20dce23940d9a57624274204c86
Yeldos Kozhabay <yeldos.kozha@gmail.com>
2025-09-25 08:12:25 +0500

    Create sidebar component

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,45 @@
+import "./styles.css";
+import { ReactNode, useState } from "react";
+
+type MenuItem = {
+  key: string;
+  label: string;
+  icon: ReactNode;
+  onClick?: () => void;
+};
+
+type Props = {
+  items: MenuItem[];
+};
+
+const Sidebar = ({ items }: Props) => {
+  const [selectedItem, setSelectedItem] = useState<string>();
+
+  const handleClick = (item: MenuItem) => {
+    setSelectedItem(item.key);
+    if (item.onClick) item.onClick();
+  };
+
+  return (
+    <aside className="sidebar">
+      <nav>
+        <ul className="list">
+          {items.map((i) => (
+            <li
+              key={i.key}
+              className={`sidebar-item${
+                selectedItem === i.key ? " selected" : ""
+              }`}>
+              <a onClick={() => handleClick(i)}>
+                {i.icon}
+                {i.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default Sidebar;
--- src/routes/private/index.tsx
+++ src/routes/private/index.tsx
@@ -1,11 +1,31 @@
 import { Outlet } from "react-router-dom";
 import "./styles.css";
+import Sidebar from "@/components/sidebar";
+import { History, Home, TrendingUp } from "lucide-react";
 
 export default function MainLayout() {
   return (
     <div className="main">
+      <Sidebar
+        items={[
+          {
+            key: "home",
+            icon: <Home />,
+            label: "Home",
+          },
+          {
+            key: "trending",
+            icon: <TrendingUp />,
+            label: "Trending",
+          },
+          {
+            key: "history",
+            icon: <History />,
+            label: "History",
+          },
+        ]}
+      />
       <Outlet />
     </div>
   );
 }
-
